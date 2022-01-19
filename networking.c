32cedffa74e7c847fbd8ced22e6e7472991652d1
Tami Takada <ttakada20@stuy.edu>
2022-01-19 11:00:31 -0500

    stuff

--- networking.c
+++ networking.c
@@ -15,11 +15,45 @@ int server_setup() {
 
   free(hints);
   freeaddrinfo(results);
-  
+
   return sd;
 }
 
+int is_duplicate(char * arr, char c) {
+  while (arr) {
+    if (*arr == c) return 1;
+    arr++;
+  }
+  return 0;
+}
+
+char * generate_characters() {
+  printf("gen");
+  char * chars = malloc(sizeof(char) * 6);
+  int i;
+  for (i = 0; i < 6; i++) {
+    if (i < 2) {
+        char vowels[5] = {'a', 'e', 'i', 'o', 'u'};
+        int r = rand() % 5;
+        chars[i] = vowels[r];
+    } else {
+      char r = rand() % 26 + 97;
+      while (is_duplicate(chars, r)) {
+        r = rand() % 26 + 97;
+      }
+      chars[i] = r;
+    }
+  }
+  return chars;
+}
+
 void processing(int sd) {
+  char * chars = generate_characters();
+  int i;
+  for (i = 0; i < 6; i++) {
+    printf("%c\n", chars[i]);
+  }
+
     char msg[100];
     int n = read(sd, msg, sizeof(msg));
     if (n) {
@@ -40,16 +74,10 @@ void handle_client(int sd) {
     struct sockaddr_storage client_address;
     sock_size = sizeof(client_address);
     client_socket = accept(sd,(struct sockaddr *)&client_address, &sock_size);
-    
-    printf("Accepted client!\n");
-    
-    int pid = fork();
-    if (pid > 0) {
-      handle_client(sd);
-    } else {
-      printf("Forked!\n");
-      processing(client_socket);
-    }
+
+    printf("Accepted match!\n");
+
+    processing(client_socket);
 }
 
 void server_connect(int sd) {
@@ -64,15 +92,14 @@ int client_connect() {
   hints->ai_family = AF_INET;
   hints->ai_socktype = SOCK_STREAM;
   getaddrinfo("127.0.0.1", "9845", hints, &results);
-  
+
   int sd = socket(results->ai_family, results->ai_socktype, results->ai_protocol);
 
   connect(sd, results->ai_addr, results->ai_addrlen);
   printf("Connected to server!\n");
-    
+
   free(hints);
   freeaddrinfo(results);
 
   return sd;
 }
-
--- server.c
+++ server.c
@@ -1,6 +1,9 @@
 #include "networking.h"
+#include <time.h>
 
 int main() {
+    srand(time(NULL));
+
     int sd = server_setup();
     server_connect(sd);
 }
