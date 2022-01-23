9f44009d493f0f135819e67324a2bf938dd45082
Tami Takada <tamitakada@gmail.com>
2022-01-23 14:21:37 -0500

    added timer & score tracking

--- player.c
+++ player.c
@@ -3,4 +3,13 @@
 int main() {
   int sd = client_connect();
   client_processing(sd);
+
+  printf("Game over!\n");
+
+  char current;
+  while (read(sd, &current, sizeof(char))) {
+    printf("%c", current);
+  }
+  
+  printf("\n");
 }
