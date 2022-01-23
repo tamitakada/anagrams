c06ee068eef8cc89dfb3e3fe7c82e4ab7203c223
Tami Takada <tamitakada@gmail.com>
2022-01-23 09:39:15 -0500

    moved processing to networking file

--- player.c
+++ player.c
@@ -1,19 +1,5 @@
 #include "networking.h"
 
-void client_processing(int sd) {
-  char letters[7];
-  int n = read(sd, letters, sizeof(letters));
-  if (n) {
-    printf("Letters: %s\n", letters);
-    // client_processing(sd);
-  }
-
-  while (1) {
-    char * word = get_input();
-    write(sd, word, (i + 1) * sizeof(char));
-  }
-}
-
 int main() {
   int sd = client_connect();
   client_processing(sd);
